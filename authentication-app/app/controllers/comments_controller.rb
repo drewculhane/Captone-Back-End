a09db21a19de753b23c842026a4c65adf1693a59
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-13 21:56:43 -0400

    added columns to posts and comments for avatars

--- authentication-app/app/controllers/comments_controller.rb
+++ authentication-app/app/controllers/comments_controller.rb
@@ -49,6 +49,6 @@ class CommentsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:content, :user_avatar, :user_display)
     end
 end
--- authentication-app/db/schema.rb
+++ authentication-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_025719) do
+ActiveRecord::Schema.define(version: 2020_08_13_233047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_08_09_025719) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "post_id"
     t.integer "user_id"
+    t.string "user_avatar"
+    t.string "user_display"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -31,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_08_09_025719) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "user_avatar"
+    t.string "user_display"
   end
 
   create_table "users", force: :cascade do |t|
